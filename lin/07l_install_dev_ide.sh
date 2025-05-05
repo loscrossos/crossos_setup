334aa1c1c25350373230b9b4781e4f4565f18642
LosCrossos <165311345+loscrossos@users.noreply.github.com>
2025-05-05 20:07:24 +0200

    added arm support for codium
    Signed-off-by: LosCrossos <165311345+loscrossos@users.noreply.github.com>

--- lin/07l_install_dev_ide.sh
+++ lin/07l_install_dev_ide.sh
@@ -18,7 +18,7 @@ sudo apt install -y code
 #https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo
 wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
 #this contains a fix for amd64 systems to add arch=amd64 else the repo will produce an annoying warning about i386 packages
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs vscodium main' | sudo tee /etc/apt/sources.list.d/archive_uri-https_paulcarroty_gitlab_io_vscodium-deb-rpm-repo_debs-noble.list
+echo 'deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs vscodium main' | sudo tee /etc/apt/sources.list.d/archive_uri-https_paulcarroty_gitlab_io_vscodium-deb-rpm-repo_debs-noble.list
 sudo apt update
 sudo apt-get install -y codium
 
